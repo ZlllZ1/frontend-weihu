085dbcc096202af7d5ee39d2773d9dcf932475d0
zhouxy <15292021323@163.com>
2024-11-21 13:54:54 +0800

    feat: 调整vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,45 @@
 const { defineConfig } = require('@vue/cli-service')
+
 module.exports = defineConfig({
-  transpileDependencies: true
-  // css: {
-  //   loaderOptions: {
-  //     postcss: {
-  //       plugins: [require('tailwindcss'), require('autoprefixer')]
-  //     }
-  //   }
-  // }
+  transpileDependencies: true,
+
+  publicPath: process.env.NODE_ENV === 'production' ? '/my-project/' : '/',
+
+  outputDir: 'dist',
+
+  assetsDir: 'static',
+
+  productionSourceMap: false,
+
+  configureWebpack: {
+    optimization: {
+      splitChunks: {
+        chunks: 'all'
+      }
+    }
+  },
+
+  chainWebpack: config => {
+    config.plugin('html').tap(args => {
+      args[0].title = 'My Vue App'
+      return args
+    })
+  },
+
+  css: {
+    loaderOptions: {
+      sass: {
+        additionalData: `@import "@/styles/variables.scss";`
+      }
+    }
+  },
+
+  devServer: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+        changeOrigin: true
+      }
+    }
+  }
 })
