11c367c1082e00f9896864e686f2c1fa4602763b
zhouxy <15292021323@163.com>
2024-11-22 14:46:45 +0800

    feat: 调整request请求类

--- src/main.ts
+++ src/main.ts
@@ -4,14 +4,11 @@ import router from './router'
 import store from './store'
 import './styles/global.scss'
 import './tailwind.css'
-import request from './utils/request.js'
 import LoadingData from './components/common/loading-data/LoadingData.vue'
 
 const app = createApp(App)
 
 app.use(router).use(store)
-
-app.config.globalProperties.$request = request
 
 app.component('LoadingData', LoadingData)
 
